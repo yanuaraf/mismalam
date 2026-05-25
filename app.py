a042a8982a562d46ba8316f64fe826dc951acc8e
yanuaraf <yanuarachmad10@gmail.com>
2026-05-25 20:51:19 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# Mengatur judul tab browser
+st.set_page_config(page_title="Aplikasi Pertamaku", page_icon="1")
+
+# Menampilkan judul dan teks di web
+st.title("Aplikasi Streamlit Pertamaku!")
+st.write("Halo dunia! Jika kamu bisa melihat halaman ini, berarti kamnu sudah **BERHASIL** meng-upload dan mendeploy aplikasi Streamlit dari Github.")
+
+st.divider() #Garis pembatas
+
+# Input sederhana
+nama = st.text_input("Siapa namamu?")
+
+#Tombol interaktif
+if st.button("Klik Saya!"):
+    if nama:
+        st.succes(f"Halo, {nama}! Selamat belajar Streamlit, kamu hebat!")
+        st.balloons() #Memunculkan animasi balon
+    else:
+        st.warning("Isi namamu dulu di kotak atas ya!")
